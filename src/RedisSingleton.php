e4090b5947014d1c02394ad5b56f6008e580da64
Aleksandr Avtomonov <avtomon@users.noreply.github.com>
2017-11-24 14:01:23 +0300

    Create RedisSingleton.php

---
+++ src/RedisSingleton.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace avtomon;
+
+class RedisSingletonException extends \Exception
+{
+}
+
+class RedisSingleton
+{
+    private static $instance = null;
+
+    public static function create($socket = '/var/run/redis.sock')
+    {
+        if (!self::$instance) {
+            $redis = new \Redis();
+            if ($redis->connect($socket)) {
+                self::$instance = $redis;
+            } else {
+                throw new RedisSingletonException ('Не удалось подключиться к сокету Redis');
+            }
+        }
+        return self::$instance;
+    }
+}
